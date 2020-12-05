210c56d9fc98036be9e3e5f70d8520c5be3ac768
gernb <gern@1dot0.net>
2020-12-04 19:22:34 -1000

    Day 5, part 1

--- Day 05/main.swift
+++ Day 05/main.swift
@@ -9,8 +9,58 @@ import Foundation
 
 // MARK: - Part 1
 
-print("Part 1:")
+enum Part1 {
+    static func seatId(for pass: [String]) -> Int {
+        var row = 0...127
+        var column = 0...7
+
+        pass.forEach { character in
+            switch character {
+            case "F":
+                let range = row.upperBound - row.lowerBound
+                row = (row.lowerBound) ... (row.lowerBound + range / 2)
+            case "B":
+                let range = row.upperBound - row.lowerBound
+                row = (row.lowerBound + range / 2 + 1) ... (row.upperBound)
+            case "L":
+                let range = column.upperBound - column.lowerBound
+                column = (column.lowerBound) ... (column.lowerBound + range / 2)
+            case "R":
+                let range = column.upperBound - column.lowerBound
+                column = (column.lowerBound + range / 2 + 1) ... (column.upperBound)
+            default:
+                fatalError()
+            }
+        }
+        return row.lowerBound * 8 + column.lowerBound
+    }
+
+    static func run(_ source: InputData) {
+        let input = source.data
+        let seatIds = input.map(seatId)
+
+        print("Part 1 (\(source)):")
+        if (source != .challenge) {
+            seatIds.enumerated().forEach {
+                print("\(input[$0]) -> \($1)")
+            }
+        }
+        print("Highest seat ID is: \(seatIds.max()!)")
+    }
+}
+
+InputData.allCases.forEach(Part1.run)
 
 // MARK: - Part 2
 
-print("Part 2:")
+print("")
+
+enum Part2 {
+    static func run(_ source: InputData) {
+        let input = source.data
+
+        print("Part 2 (\(source)):")
+    }
+}
+
+InputData.allCases.forEach(Part2.run)
