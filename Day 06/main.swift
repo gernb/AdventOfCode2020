9bade9998f6c1fd17bc1d33749513cee42a8fed7
gernb <gern@1dot0.net>
2020-12-05 19:42:39 -1000

    Late start. Day 6, part 1

--- Day 06/main.swift
+++ Day 06/main.swift
@@ -9,8 +9,31 @@ import Foundation
 
 // MARK: - Part 1
 
-print("Part 1:")
+enum Part1 {
+    static func uniqueAnswers(for group: String) -> Int {
+        Set(group.replacingOccurrences(of: "\n", with: "").map(String.init)).count
+    }
+
+    static func run(_ source: InputData) {
+        let input = source.data
+        let sum = input.map(uniqueAnswers).reduce(0, +)
+        print("Part 1 (\(source)):")
+        print("Sum of unique answers is: \(sum)")
+    }
+}
+
+InputData.allCases.forEach(Part1.run)
 
 // MARK: - Part 2
 
-print("Part 2:")
+print("")
+
+enum Part2 {
+    static func run(_ source: InputData) {
+        let input = source.data
+
+        print("Part 2 (\(source)):")
+    }
+}
+
+InputData.allCases.forEach(Part2.run)
