e7f4264197add34d746cc44b010dc2fe4f9c2205
gernb <gern@1dot0.net>
2020-12-08 22:10:40 -0800

    Day 9, part 1. Got a late start for this one...

--- Day 09/main.swift
+++ Day 09/main.swift
@@ -9,8 +9,43 @@ import Foundation
 
 // MARK: - Part 1
 
-print("Part 1:")
+print("Day 9:")
+
+enum Part1 {
+    static func isValid(number: Int, for preamble: Set<Int>) -> Bool {
+        for value in preamble {
+            if preamble.subtracting([value]).contains(number - value) {
+                return true
+            }
+        }
+        return false
+    }
+
+    static func run(_ source: InputData) {
+        let (len, input) = source.data
+        print("Part 1 (\(source)):")
+        for (idx, number) in input.enumerated().dropFirst(len) {
+            let previousNumbers = Set(input[idx - len ..< idx])
+            if !isValid(number: number, for: previousNumbers) {
+                print("Found invalid entry: \(number) at position: \(idx)")
+                break
+            }
+        }
+    }
+}
+
+InputData.allCases.forEach(Part1.run)
 
 // MARK: - Part 2
 
-print("Part 2:")
+print("")
+
+enum Part2 {
+    static func run(_ source: InputData) {
+        let input = source.data
+
+        print("Part 2 (\(source)):")
+    }
+}
+
+InputData.allCases.forEach(Part2.run)
