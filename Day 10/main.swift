32b18e460b4c5a42f633e13cbc93308cb88cdb1a
gernb <gern@1dot0.net>
2020-12-09 21:16:16 -0800

    Day 10, part 1

--- Day 10/main.swift
+++ Day 10/main.swift
@@ -9,8 +9,36 @@ import Foundation
 
 // MARK: - Part 1
 
-print("Part 1:")
+print("Day 10:")
+
+enum Part1 {
+    static func run(_ source: InputData) {
+        let input = source.data.sorted()
+        var previous = 0
+        let differences = (input + [input.max()! + 3]).map { adaptor -> Int in
+            defer { previous = adaptor }
+            return adaptor - previous
+        }
+        print("Part 1 (\(source)):")
+        print("Joltage differences: \(differences)")
+        let oneJoltDiffs = differences.filter { $0 == 1 }.count
+        let threeJoltDiffs = differences.filter { $0 == 3 }.count
+        print("Result = \(oneJoltDiffs * threeJoltDiffs)\n")
+    }
+}
+
+InputData.allCases.forEach(Part1.run)
 
 // MARK: - Part 2
 
-print("Part 2:")
+print("")
+
+enum Part2 {
+    static func run(_ source: InputData) {
+        let input = source.data
+
+        print("Part 2 (\(source)):")
+    }
+}
+
+InputData.allCases.forEach(Part2.run)
