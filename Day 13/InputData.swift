3591829f58396966d4b27f760b4c8764f0e3c0aa
gernb <gern@1dot0.net>
2020-12-12 21:10:56 -0800

    Day 13, part 1

--- Day 13/InputData.swift
+++ Day 13/InputData.swift
@@ -5,6 +5,22 @@
 //  Copyright © 2020 peter bohac. All rights reserved.
 //
 
-struct InputData {
+enum InputData: String, CaseIterable {
+    case example, challenge
 
+    var data: [String] {
+        switch self {
+
+        case .example: return """
+939
+7,13,x,x,59,x,31,19
+""".components(separatedBy: .newlines)
+
+        case .challenge: return """
+1009310
+19,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,599,x,29,x,x,x,x,x,x,x,x,x,x,x,x,x,x,17,x,x,x,x,x,23,x,x,x,x,x,x,x,761,x,x,x,x,x,x,x,x,x,41,x,x,13
+""".components(separatedBy: .newlines)
+
+        }
+    }
 }
--- Day 13/main.swift
+++ Day 13/main.swift
@@ -9,8 +9,39 @@ import Foundation
 
 // MARK: - Part 1
 
-print("Part 1:")
+print("Day 13:")
+
+enum Part1 {
+    static func run(_ source: InputData) {
+        let input = source.data
+        let timestamp = Int(input[0])!
+        let busIDs = input[1].components(separatedBy: ",").compactMap(Int.init)
+
+        print("Part 1 (\(source)):")
+        for time in timestamp... {
+            for id in busIDs {
+                if time % id == 0 {
+                    let delay = time - timestamp
+                    print("Bus ID \(id) leaves in \(delay) minutes for a product of \(id * delay)")
+                    return
+                }
+            }
+        }
+    }
+}
+
+InputData.allCases.forEach(Part1.run)
 
 // MARK: - Part 2
 
-print("Part 2:")
+print("")
+
+enum Part2 {
+    static func run(_ source: InputData) {
+        let input = source.data
+
+        print("Part 2 (\(source)):")
+    }
+}
+
+InputData.allCases.forEach(Part2.run)
