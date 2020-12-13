0508f6f8c8783f559a99e8402d37f8d75c88ce17
gernb <gern@1dot0.net>
2020-12-12 23:38:54 -0800

    Day 13, part 2 With the solution I used to get my star (WolframAlpha) and the fully automated solution.

--- Day 13/InputData.swift
+++ Day 13/InputData.swift
@@ -6,7 +6,7 @@
 //
 
 enum InputData: String, CaseIterable {
-    case example, challenge
+    case example, example2, example3, example4, example5, example6, challenge
 
     var data: [String] {
         switch self {
@@ -14,11 +14,19 @@ enum InputData: String, CaseIterable {
         case .example: return """
 939
 7,13,x,x,59,x,31,19
+1068781
 """.components(separatedBy: .newlines)
+
+        case .example2: return ["", "17,x,13,19", "3417"]
+        case .example3: return ["", "67,7,59,61", "754018"]
+        case .example4: return ["", "67,x,7,59,61", "779210"]
+        case .example5: return ["", "67,7,x,59,61", "1261476"]
+        case .example6: return ["", "1789,37,47,1889", "1202161486"]
 
         case .challenge: return """
 1009310
 19,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,599,x,29,x,x,x,x,x,x,x,x,x,x,x,x,x,x,17,x,x,x,x,x,23,x,x,x,x,x,x,x,761,x,x,x,x,x,x,x,x,x,41,x,x,13
+1012171816131114
 """.components(separatedBy: .newlines)
 
         }
--- Day 13/main.swift
+++ Day 13/main.swift
@@ -30,18 +30,100 @@ enum Part1 {
     }
 }
 
-InputData.allCases.forEach(Part1.run)
+Part1.run(.example)
+Part1.run(.challenge)
 
 // MARK: - Part 2
 
 print("")
 
+func gcd(_ m: Int, _ n: Int) -> Int {
+    var a: Int = 0
+    var b: Int = max(m, n)
+    var r: Int = min(m, n)
+
+    while r != 0 {
+        a = b
+        b = r
+        r = a % b
+    }
+    return b
+}
+
+func lcm(_ m: Int, _ n: Int) -> Int {
+    return (m * n) / gcd(m, n)
+}
+
 enum Part2 {
-    static func run(_ source: InputData) {
+    // Doesn't work on the challenge input
+    static func bruteForce(_ source: InputData) {
         let input = source.data
+        let busIDs = input[1].components(separatedBy: ",").map(Int.init)
+        let result = input.count == 3 ? Int(input[2])! : -1
+
+        let start: Int
+        if source == .challenge {
+            start = (100000000000000 / busIDs[0]!) * busIDs[0]!
+        } else {
+            start = 0
+        }
 
         print("Part 2 (\(source)):")
+        for time in stride(from: start, to: Int.max, by: busIDs[0]!) {
+            let mods = busIDs.indices.compactMap { idx -> Int? in
+                guard let id = busIDs[idx] else { return nil }
+                return (time + idx) % id
+            }
+            if mods.allSatisfy({ $0 == 0 }) {
+                print("Timestamp: \(time) == \(result)? \(time == result)")
+                return
+            }
+        }
+    }
+
+    // Increase the step size by the LCM of the bus IDs (step size for the bus) every time they match/align
+    static func run(_ source: InputData) {
+        let input = source.data
+        let busses = input[1]
+            .components(separatedBy: ",")
+            .map(Int.init)
+            .enumerated()
+            .compactMap { idx, id -> (idx: Int, id: Int)? in
+                guard let id = id else { return nil }
+                return (idx, id)
+            }
+        let result = input.count == 3 ? Int(input[2])! : -1
+
+        print("Part 2 (\(source)):")
+        var time = 0
+        var step = busses[0].id
+        while true {
+            let matches = busses.compactMap {
+                (time + $0.idx) % $0.id == 0 ? $0.id : nil
+            }
+            if matches.count == busses.count {
+                print("Timestamp: \(time) == \(result)? \(time == result)")
+                return
+            }
+            if matches.count > 1 {
+                step = matches.reduce(1, lcm)
+            }
+            time += step
+        }
+    }
+
+    // Alternate solution:
+    // Paste this string into WolframAlpha to get the answer...
+    static func wolframAlpha(_ source: InputData) {
+        let input = source.data
+        let busIDs = input[1].components(separatedBy: ",").map(Int.init)
+        let string = busIDs.enumerated().compactMap { idx, id -> String? in
+            guard let id = id else { return nil }
+            return "(t + \(idx)) mod \(id) = 0"
+        }.joined(separator: ", ")
+        print(string)
     }
 }
 
 InputData.allCases.forEach(Part2.run)
+//InputData.allCases.forEach(Part2.wolframAlpha)
