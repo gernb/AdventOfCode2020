6698d84a40a5b0e7c7b0887d5e49c34a9b984aed
gernb <gern@1dot0.net>
2020-12-14 21:11:07 -0800

    Day 15, part 1

--- Day 15/InputData.swift
+++ Day 15/InputData.swift
@@ -5,6 +5,16 @@
 //  Copyright © 2020 peter bohac. All rights reserved.
 //
 
-struct InputData {
+enum InputData: String, CaseIterable {
+    case example, challenge
 
+    var data: [Int] {
+        switch self {
+
+        case .example: return "0,3,6".components(separatedBy: ",").compactMap(Int.init)
+
+        case .challenge: return "19,0,5,1,10,13".components(separatedBy: ",").compactMap(Int.init)
+
+        }
+    }
 }
--- Day 15/main.swift
+++ Day 15/main.swift
@@ -9,8 +9,35 @@ import Foundation
 
 // MARK: - Part 1
 
-print("Part 1:")
+print("Day 15:")
+
+enum Part1 {
+    static func run(_ source: InputData) {
+        var input = source.data
+        for turn in input.count ..< 2020 {
+            let last = input.last!
+            if let previous = input.dropLast().lastIndex(of: last) {
+                input.append(turn - previous - 1)
+            } else {
+                input.append(0)
+            }
+        }
+        print("Part 1 (\(source)): 2020th number is \(input.last!)")
+    }
+}
+
+InputData.allCases.forEach(Part1.run)
 
 // MARK: - Part 2
 
-print("Part 2:")
+print("")
+
+enum Part2 {
+    static func run(_ source: InputData) {
+        let input = source.data
+
+        print("Part 2 (\(source)):")
+    }
+}
+
+InputData.allCases.forEach(Part2.run)
