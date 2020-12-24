60f3c5c6b9798013eeeeff856e3bd47198ad08a0
gernb <gern@1dot0.net>
2020-12-24 12:08:39 -0800

    Day 22, part 1

--- Day 22/InputData.swift
+++ Day 22/InputData.swift
@@ -5,6 +5,84 @@
 //  Copyright © 2020 peter bohac. All rights reserved.
 //
 
-struct InputData {
+enum InputData: String, CaseIterable {
+    case example, challenge
 
+    var data: [[String]] {
+        switch self {
+
+        case .example: return """
+Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10
+""".components(separatedBy: "\n\n").map { $0.components(separatedBy: .newlines) }
+
+        case .challenge: return """
+Player 1:
+44
+24
+36
+6
+27
+46
+33
+45
+47
+41
+15
+23
+40
+38
+43
+42
+25
+5
+30
+35
+34
+13
+29
+1
+50
+
+Player 2:
+32
+28
+4
+12
+9
+21
+48
+18
+31
+39
+20
+16
+3
+37
+49
+7
+17
+22
+8
+26
+2
+14
+11
+19
+10
+""".components(separatedBy: "\n\n").map { $0.components(separatedBy: .newlines) }
+
+        }
+    }
 }
--- Day 22/main.swift
+++ Day 22/main.swift
@@ -9,8 +9,44 @@ import Foundation
 
 // MARK: - Part 1
 
-print("Part 1:")
+print("Day 22:")
+
+enum Part1 {
+    static func run(_ source: InputData) {
+        let input = source.data
+        var player1 = input[0].dropFirst().compactMap(Int.init)
+        var player2 = input[1].dropFirst().compactMap(Int.init)
+
+        while !player1.isEmpty && !player2.isEmpty {
+            let p1 = player1.removeFirst()
+            let p2 = player2.removeFirst()
+            if p1 > p2 {
+                player1.append(contentsOf: [p1, p2])
+            } else {
+                player2.append(contentsOf: [p2, p1])
+            }
+        }
+
+        let winner = player1.isEmpty ? player2 : player1
+        let score = winner.reversed().enumerated().reduce(0) { (sum, pair) in
+            sum + (pair.offset + 1) * pair.element
+        }
+        print("Part 1 (\(source)): \(score)")
+    }
+}
+
+InputData.allCases.forEach(Part1.run)
 
 // MARK: - Part 2
 
-print("Part 2:")
+print("")
+
+enum Part2 {
+    static func run(_ source: InputData) {
+        let input = source.data
+
+        print("Part 2 (\(source)):")
+    }
+}
+
+InputData.allCases.forEach(Part2.run)
